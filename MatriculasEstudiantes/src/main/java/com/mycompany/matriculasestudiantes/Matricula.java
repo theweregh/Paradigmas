0655c4a681a84874102195cf55b98664fa4c8180
theweregh <theweregh@gmail.com>
2023-10-20 20:05:37 -0500

    corrección matriculas estudiantes

--- MatriculasEstudiantes/src/main/java/com/mycompany/matriculasestudiantes/Matricula.java
+++ MatriculasEstudiantes/src/main/java/com/mycompany/matriculasestudiantes/Matricula.java
@@ -1,13 +1,11 @@
 package com.mycompany.matriculasestudiantes;
 public class Matricula {
-    private String idEstudiante,idAsignatura,fecha,asigMenosRegistrados,asigMasRegistrados;
+    private String idEstudiante,idAsignatura,fecha;
 
-    public Matricula(String idEstudiante, String idAsignatura, String fecha, String asigMenosRegistrados, String asigMasRegistrados) {
+    public Matricula(String idEstudiante, String idAsignatura, String fecha) {
         this.idEstudiante = idEstudiante;
         this.idAsignatura = idAsignatura;
         this.fecha = fecha;
-        this.asigMenosRegistrados = asigMenosRegistrados;
-        this.asigMasRegistrados = asigMasRegistrados;
     }
 
     public String getIdEstudiante() {
@@ -33,22 +31,5 @@ public class Matricula {
     public void setFecha(String fecha) {
         this.fecha = fecha;
     }
-
-    public String getAsigMenosRegistrados() {
-        return asigMenosRegistrados;
-    }
-
-    public void setAsigMenosRegistrados(String asigMenosRegistrados) {
-        this.asigMenosRegistrados = asigMenosRegistrados;
-    }
-
-    public String getAsigMasRegistrados() {
-        return asigMasRegistrados;
-    }
-
-    public void setAsigMasRegistrados(String asigMasRegistrados) {
-        this.asigMasRegistrados = asigMasRegistrados;
-    }
-    
-    
+ 
 }
--- MatriculasEstudiantes/src/main/java/com/mycompany/matriculasestudiantes/MatriculasEstudiantes.java
+++ MatriculasEstudiantes/src/main/java/com/mycompany/matriculasestudiantes/MatriculasEstudiantes.java
@@ -10,10 +10,55 @@ public class MatriculasEstudiantes {
     static List<Bienestar> cursosBienestar = new ArrayList<Bienestar>();
     static List<Matricula> matriculas = new ArrayList<Matricula>();
     public static void main(String[] args) {
-        
+        int otro=1,otro1,n=0;
+        estudiantes.add(new Estudiante("1234","camilo","Fernandez","1/04/2010", "camilo@gmail.com"));
+        asignaturas.add(new Asignatura(1, "0", "Paradigmas de programación", "Arquitectura de software", 0));
+        cursosBienestar.add(new Bienestar(1, "1", "ingles", "centro de lenguas", 0));
+        while(otro==1){
+            n= Integer.parseInt(JOptionPane.showInputDialog("1.ingresar estudiante\n2.ingresar asignatura\n3.ingresar asignatura bienestar\n4.crear matricula\n5.crear descuento\n6.mostrar asignaturas con mayor y menor registrados\n7.salir"));
+            switch(n){
+                case 1:
+                    otro1=1;
+                    while(otro1==1){
+                    estudiantes.add(new Estudiante(JOptionPane.showInputDialog("ingrese el id del estudiante"),JOptionPane.showInputDialog("ingrese el nombre del estudiante"), JOptionPane.showInputDialog("ingrese el apellido del estudiante"), JOptionPane.showInputDialog("ingrese la fecha de nacimiento del estudiante"),JOptionPane.showInputDialog("ingrese el email del estudiante")));
+                    otro1= Integer.parseInt(JOptionPane.showInputDialog("ingrese 1 para continuar de lo contrario otro numero"));
+                    }
+                    break;
+                case 2:
+                    otro1=1;
+                    while(otro1==1){
+                    asignaturas.add(new Asignatura(Float.parseFloat(JOptionPane.showInputDialog("ingrese el numero de creditos de la asignatura")),JOptionPane.showInputDialog("ingrese el id de la asignatura"), JOptionPane.showInputDialog("ingrese el nombre de la asignatura"), JOptionPane.showInputDialog("ingrese la linea de la asignatura"), 0));
+                    otro1= Integer.parseInt(JOptionPane.showInputDialog("ingrese 1 para continuar de lo contrario otro numero"));
+                    }
+                    break;
+                case 3:
+                otro1=1;
+                    while(otro1==1){
+                    cursosBienestar.add(new Bienestar(Float.parseFloat(JOptionPane.showInputDialog("ingrese la cantidad de horas del curso de bienestar")), JOptionPane.showInputDialog("ingrese el id de la asignatura"),  JOptionPane.showInputDialog("ingrese el nombre de la asignatura"), JOptionPane.showInputDialog("ingrese la linea de la asignatura"), 0));
+                    otro1= Integer.parseInt(JOptionPane.showInputDialog("ingrese 1 para continuar de lo contrario otro numero"));
+                    }
+                    break;
+                case 4:
+                otro1=1;
+                    while(otro1==1){
+                    matriculas.add(new Matricula(JOptionPane.showInputDialog("ingrese el id del estudiante"),  JOptionPane.showInputDialog("ingrese el id de la asignatura"),  JOptionPane.showInputDialog("ingrese la fecha de la matricula")));
+                    otro1= Integer.parseInt(JOptionPane.showInputDialog("ingrese 1 para continuar de lo contrario otro numero"));
+                    }
+                    break;
+                case 5:
+                    
+                    break;
+                case 6:
+                    MatriculasEstudiantes.buscarMayorMenorRegistrado();
+                    break;
+                case 7:
+                    otro=0;
+                    break;
+            }
+        }
     }
-    static public void buscarMayorRegistrado(){
-        int i=0,j=0,cAsignatura=0,cBienestar=0;//String idMayorAsignaturas,idMayorBienestar;
+    static public void buscarMayorMenorRegistrado(){
+        int i=0,j=0;//String idMayorAsignaturas,idMayorBienestar;
         while(j<asignaturas.size()){
             if(matriculas.get(i).getIdAsignatura().equals(asignaturas.get(j).getIdAsignatura())){
                 asignaturas.get(j).setContador(asignaturas.get(j).getContador()+1);
@@ -29,6 +74,11 @@ public class MatriculasEstudiantes {
             i++;
             j++;
         }
+        MatriculasEstudiantes.buscarMayorRegistrado();
+        MatriculasEstudiantes.buscarMenorRegistrado();
+    }
+    static public void buscarMayorRegistrado(){
+        int cAsignatura=0,cBienestar=0;//contadores
         for(int k=0;k<asignaturas.size();k++){
             if(k==0){
                 //idMayorAsignaturas = asignaturas.get(k).getIdAsignatura();
@@ -59,5 +109,54 @@ public class MatriculasEstudiantes {
         if(cAsignatura<cBienestar){
             JOptionPane.showMessageDialog(null,"la asignatura con mayor numero de registros es: "+cBienestar);
         }
+    }
+    static public void buscarMenorRegistrado(){
+        int cAsignatura=0,cBienestar=0;//contadores
+        for(int k=0;k<asignaturas.size();k++){
+            if(k==0){
+                //idMayorAsignaturas = asignaturas.get(k).getIdAsignatura();
+                cAsignatura = k;
+            }
+            if(k>0){
+                if(asignaturas.get(cAsignatura).getContador()>asignaturas.get(k).getContador()){
+                    //idMayorAsignaturas = asignaturas.get(k).getIdAsignatura();
+                    cAsignatura = k;
+                }
+            }
+        }
+        for(int k=0;k<cursosBienestar.size();k++){
+            if(k==0){
+                //idMayorBienestar = cursosBienestar.get(k).getIdAsignatura();
+                cBienestar = k;
+            }
+            if(k>0){
+                if(cursosBienestar.get(cAsignatura).getContador()>cursosBienestar.get(k).getContador()){
+                    //idMayorBienestar = cursosBienestar.get(k).getIdAsignatura();
+                    cBienestar = k;
+                }
+            }
+        }
+        if(cAsignatura<cBienestar){
+            JOptionPane.showMessageDialog(null,"la asignatura con menor numero de registros es: "+cAsignatura);
+        }
+        if(cAsignatura>cBienestar){
+            JOptionPane.showMessageDialog(null,"la asignatura con menor numero de registros es: "+cBienestar);
+        }
+    }
+    static public String BuscarEstudiante(){
+        String id,idEncontrado="";int otro=1,i=0;
+        id = JOptionPane.showInputDialog("ingrese el id del estudiante");
+        while(otro==1){
+            if(id.equalsIgnoreCase(estudiantes.get(i).getId())){
+                idEncontrado =estudiantes.get(i).getId();
+                otro=0;
+            }
+            if(i==estudiantes.size()){
+                estudiantes.add(new Estudiante(JOptionPane.showInputDialog("ingrese el id del estudiante"),JOptionPane.showInputDialog("ingrese el nombre del estudiante"), JOptionPane.showInputDialog("ingrese el apellido del estudiante"), JOptionPane.showInputDialog("ingrese la fecha de nacimiento del estudiante"),JOptionPane.showInputDialog("ingrese el email del estudiante")));
+                idEncontrado =estudiantes.get(i).getId();
+                otro=0;
+            }
+        }
+        return idEncontrado;
     }
 }
